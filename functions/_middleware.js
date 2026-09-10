0f561fa142ffa6515b2679432af4c806b92bd0fb
ranisamer <rani.samer@gmail.com>
2026-09-10 10:59:33 +0400

    Add Google Analytics 4 tracking sitewide via Pages middleware

---
+++ functions/_middleware.js
@@ -0,0 +1,27 @@
+const GA_MEASUREMENT_ID = "G-T47EEK5EH0";
+
+export async function onRequest(context) {
+  const response = await context.next();
+  const contentType = response.headers.get("content-type") || "";
+
+  if (!contentType.includes("text/html")) {
+    return response;
+  }
+
+  return new HTMLRewriter()
+    .on("head", {
+      element(element) {
+        element.append(
+          `<script async src="https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', '${GA_MEASUREMENT_ID}');
+</script>`,
+          { html: true }
+        );
+      }
+    })
+    .transform(response);
+}
